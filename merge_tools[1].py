2ad1ebc5aca4de1b9faa188c3bf53037ee9d7143
swethamani25 <92358649+swethamani25@users.noreply.github.com>
2021-10-17 12:59:19 +0530

    Add files via upload

---
+++ merge_tools[1].py
@@ -0,0 +1,11 @@
+def merge_the_tools(string, k):
+    from collections import OrderedDict
+    # your code goes here
+    strlen = len(string)
+    for i in range(0,strlen,k):
+        print(''.join(OrderedDict.fromkeys(string[i:i + k])))
+        
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
